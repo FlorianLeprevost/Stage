d0fb91a6001782e23baaf6df1eb1768c64b99744
Etudiant <florian.leprevost@gmail.com>
2019-11-22 14:27:29 +0100

    this should work : better outlier remove + keep fieldtrip structure

--- EDA.m
+++ EDA.m
@@ -40,45 +40,69 @@ plot(stats.x_spectre, stats.RR)
 
 
 pause
-%%
-%en supprimant les trials qui depasse la moyenne + 5*sd à au moins un sample
+%% remove outliers
+
+%en supprimant les trials hors d'un interval
+
 %get outliers
-trials = data_final_trials.trial;
+clean_trials = data_final_trials;
 labels = string(data_final_trials.label);
-var = data_final_average.var;
-avg = data_final_average.avg;
-for n=1:length(labels)
-    trials_str(n).data = squeeze(trials(:,n,:))
-    trials_str(n).label = labels(n)
-    trials_str(n).avg = avg(n,:)
-    trials_str(n).var = var(n,:)
-end
+% var = data_final_average.var;
+% avg = data_final_average.avg;
 
 % test visuellement quelle mesure prendre pour outliers
 for n=1:length(labels)
     figure
-    plot(data_final_trials.time, trials_str(n).data)
-    hold on
-
-    plot(data_final_average.time, (trials_str(n).avg + 5*sqrt(trials_str(n).var)))
-    hold on
-    plot(data_final_average.time, (trials_str(n).avg - 5*sqrt(trials_str(n).var)))
-
-    hold on
-    plot(data_final_average.time, (trials_str(n).avg + trials_str(n).var))
-    hold on
-    plot(data_final_average.time, (trials_str(n).avg - trials_str(n).var))
+    plot(clean_trials.time, squeeze(clean_trials.trial(:,n,:)))
 end
 
-%remove outliers
+
+%transformation de la matric de chaque electrode en une seule colonne pour
+%mieux determiner la limite des outliers
+arrayed_channels ={};
+for n=1:length(labels);
+    mat_chan = squeeze(clean_trials.trial(:,n,:));
+    arrayed_channels{n}=mat_chan(:);
+end
+
+%determiner l'interval
+for n=1:length(labels)
+    variance = nanvar(arrayed_channels{n})
+    moyenne = nanmean(arrayed_channels{n})
+    
+    ok = 0
+    outlier_lim = 0.001
+    disp(['current exclusion limit = '+ string(outlier_lim)])
+    while ok==0
+        figure
+        histogram(arrayed_channels{n}, 50)
+        title('distribution of amplitudes in ' + labels(n))
+        hold on
+        y= ylim;
+        CI = quantile(arrayed_channels{n},[outlier_lim/2 1-outlier_lim/2])
+        plot([CI(1) CI(1)], [y(1) y(2)])  
+        hold on
+        plot([CI(2) CI(2)], [y(1) y(2)])     
+        decision = input ('do you want to exclude trials outside this interval (0)?\n do you want to keep all trials ?(1)\n do you want to exclude trials outside a difference interval (answer your limit)?')
+        if decision == 0
+            ok=1
+        elseif decision == 1
+            break
+        else
+            outlier_lim=decision
+        end
+    end  
+end
+
+
+%get outlier trials
 bad_trials = [ ]
 for n=1:length(labels)
-    for tr = 1:length(trials_str(n).data(:,1))
-        for sample=1:length(trials_str(n).data(1,:))
+    CI = quantile(arrayed_channels{n},[outlier_lim/2 1-outlier_lim/2])
+    for tr = 1:length(clean_trials.trial(:,1,1))
+        for sample=1:length(clean_trials.trial(1,1,:))
             %supprime le trial if condition pas respectée
-            bornep = trials_str(n).avg(sample) + 5*sqrt(trials_str(n).var(sample));
-            bornem = trials_str(n).avg(sample) - 5*sqrt(trials_str(n).var(sample));
-            if (trials_str(n).data(tr,sample) > bornep) || (trials_str(n).data(tr,sample) < bornem)
+            if (clean_trials.trial(tr,n,sample) > CI(2) || clean_trials.trial(tr,n,sample) < CI(1))
                 bad_trials=[bad_trials tr];
                 break
             end
@@ -91,22 +115,29 @@ end
 bad_trials_ok = sort(unique(bad_trials))
 
 % supprime outliers
-figure
-for n=1:length(labels)
-    trials_str(n).data(bad_trials_ok,:)=[]
-    figure
-    plot(data_final_trials.time, trials_str(n).data)
-end
-save(['data_' patient_number '_stats' ], 'trials_str','bad_trials_ok', '-append')
+all_trials = 1:length(clean_trials.trial(:,1,1))
+good_trials = setdiff(all_trials,bad_trials_ok)
 
-%ET supprimer les trials outliers
+cfg =[]
+cfg.trials= good_trials
+clean_trials = ft_selectdata(cfg, clean_trials)
+
+save(['data_' patient_number '_' macro_name '_stats' ], 'clean_trials','bad_trials_ok')
+
+%ET supprimer les trials outliers dans les info sur le coeur
+ibi_post = [event_ok.interval_post]
+ibi_pre = [event_ok.interval_pre]
+diff_ibi = [event_ok.interval_diff]
+
 ibi_post(bad_trials_ok) = []
 ibi_pre(bad_trials_ok) = []
 diff_ibi(bad_trials_ok) = []
 
 
-%%
-%last part = plot bined data by criterion
+%% last part = plot bined data by criterion
+nb_col =length(clean_trials.trial(1,1,:))
+
+
 nb_bins = input('how many bins?')
 name_test =[]
 for tri = 1:3
@@ -132,18 +163,18 @@ for tri = 1:3
     fdiff= figure('Name', ['difference between bins' + string(name_test)])
     fcolor = figure('Name', ['colors' + string(name_test)])
     
-    for dip = 1:length(trials_str)
-        channel = trials_str(dip).data;
-        label = trials_str(dip).label;
+    for dip = 1:length(labels)
+        channel = clean_trials(:,dip,:);
+        label =labels(dip);
         %rajoute la colonne d'ordre dans sorted trials
-        channel(:,2002:2004) = sorted_var(:,1:3);
+        channel(:,nb_col+1:nb_col+3) = sorted_var(:,1:3);
         %ranger les trials en fction de ce vecteur
-        channel = sortrows(channel, 2004);
+        channel = sortrows(channel, nb_col+3);
 
         %remove lines with NaN
         for n=1:length(channel(:,1))
             try
-                if any(isnan(channel(n,2002)))
+                if any(isnan(channel(n,nb_col+1)))
                     disp('isnan')
                     channel(n,:)=[];
                 end
@@ -151,7 +182,7 @@ for tri = 1:3
         end
 
         %clean
-        channel(:,2002:2004)=[];
+        channel(:,nb_col+1:nb_col+3)=[];
         
         %bin data
         bornem = 1
@@ -168,19 +199,20 @@ for tri = 1:3
 % 
 %       plot superposed
         figure(fchan)
-        subplot(length(trials_str),1,dip)
+        subplot(length(labels),1,dip)
         plot(data_final_trials.time, bin_sorted)
         title('All trials of ' + string(label) + ' binned by ' + string(nb_bins) + ' sorted by ' + string(name_test))
 % 
         figure(fdiff)
-        subplot(length(trials_str),1,dip)
+        subplot(length(labels),1,dip)
         plot(data_final_trials.time, diff(bin_sorted))
         
         figure(fcolor)
-        subplot(length(trials_str),1,dip)
+        subplot(length(labels),1,dip)
         image(channel)
-        %plot with offset4
-%         offset=1
+        
+        %plot with offset
+%         offset=5
 %         offset_vector = (offset:offset:nb_bins*offset)';
 %         bin_plus_offset = bsxfun(@plus,bin_sorted,offset_vector);
 %         subplot(3,2,3:6)
