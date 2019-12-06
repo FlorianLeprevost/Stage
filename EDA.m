f6d4df23bab89be61c6bb3a07fb9a4c35cb3b932
Etudiant <florian.leprevost@gmail.com>
2019-12-06 11:16:25 +0100

    better pcolor + get latence and amplitude of peaks

--- EDA.m
+++ EDA.m
@@ -53,6 +53,7 @@ for n=1:length(labels)
     figure
     plot(clean_trials.time, squeeze(clean_trials.trial(:,n,:)))
 end
+title('All trials')
 
 
 %transformation de la matric de chaque electrode en une seule colonne pour
@@ -120,6 +121,11 @@ cfg =[]
 cfg.trials= good_trials
 clean_trials = ft_selectdata(cfg, clean_trials)
 
+for n=1:length(labels)
+    figure
+    plot(clean_trials.time, squeeze(clean_trials.trial(:,n,:)))
+end
+
 save(['data_' patient_number '_' macro_name '_stats' ], 'clean_trials','bad_trials_ok')
 
 %ET supprimer les trials outliers dans les info sur le coeur
@@ -138,23 +144,31 @@ nb_col =length(clean_trials.trial(1,1,:))
 
 nb_bins = input('how many bins?')
 name_test =[]
-for tri = 1:3
-    if tri == 1
+for tri = 1:4
+    if tri ==1
+        name_test = 'order of apparition'
+        field_name = 'order'
+    elseif tri == 2
         var = transpose(ibi_post)
         name_test = 'interval to Next IBI'
-    elseif tri == 2
+        field_name = 'next_ibi'       
+    elseif tri == 3
         var = transpose(ibi_pre)
         name_test = 'interval to Previous IBI'
-    elseif tri==3 
+        field_name = 'prev_ibi'       
+    elseif tri== 4
         var= transpose(diff_ibi)
         name_test = 'difference of previous and next interval '
+        field_name = 'diff_ibi'
     end
 
+    if tri ~= 1
     %sort by var
-    var(:,2) = 1:length(var)
-    sorted_var = sortrows(var);
-    sorted_var(:,3) = 1:length(var);
-    sorted_var = sortrows(sorted_var,2);
+        var(:,2) = 1:length(var)
+        sorted_var = sortrows(var);
+        sorted_var(:,3) = 1:length(var);
+        sorted_var = sortrows(sorted_var,2);
+    end
 
     %sort trials
     fchan = figure('Name', string(name_test))
@@ -164,23 +178,27 @@ for tri = 1:3
     for dip = 1:length(labels)
         channel = squeeze(clean_trials.trial(:,dip,:));
         label =labels(dip);
-        %rajoute la colonne d'ordre dans sorted trials
-        channel(:,nb_col+1:nb_col+3) = sorted_var(:,1:3);
-        %ranger les trials en fction de ce vecteur
-        channel = sortrows(channel, nb_col+3);
+        if tri ~=1
+            %rajoute la colonne d'ordre dans sorted trials
+            channel(:,nb_col+1:nb_col+3) = sorted_var(:,1:3);
+            %ranger les trials en fction de ce vecteur
+            channel = sortrows(channel, nb_col+3);
 
-        %remove lines with NaN
-        for n=1:length(channel(:,1))
-            try
-                if any(isnan(channel(n,nb_col+1)))
-                    disp('isnan')
-                    channel(n,:)=[];
+            %remove lines with NaN
+            for n=1:length(channel(:,1))
+                try
+                    if any(isnan(channel(n,nb_col+1)))
+                        disp('isnan')
+                        channel(n,:)=[];
+                    end
                 end
             end
         end
 
+        if tri ~=1
         %clean
-        channel(:,nb_col+1:nb_col+3)=[];
+            channel(:,nb_col+1:nb_col+3)=[];
+        end
         
         %bin data
         bornem = 1
@@ -194,21 +212,34 @@ for tri = 1:3
             bin_sorted(n,:) = mean(channel(bornem:bornep,:))
             bornem=bornep
         end
+        
+        for n=1:nb_bins
+            [pks, locs] = findpeaks(bin_sorted(n,:))
+            [peak, lat] = max(pks)
+            lat= locs(lat)
+            peaks_amp_lat.(field_name(1)).(char(labels_field{dip})) = [peak, lat]
+        end
 % 
 %       plot superposed
         figure(fchan)
         subplot(length(labels),1,dip)
-        plot(data_final_trials.time, bin_sorted)
+        plot(clean_trials.time, bin_sorted)
         title('All trials of ' + string(label) + ' binned by ' + string(nb_bins) + ' sorted by ' + string(name_test))
-% 
-        figure(fdiff)
-        subplot(length(labels),1,dip)
-        plot(data_final_trials.time, diff(bin_sorted))
+        xlim([-.2 .7])
+        
+%         figure(fdiff)
+%         subplot(length(labels),1,dip)
+%         plot(data_final_trials.time, diff(bin_sorted))
+%         xlim([-.2 .7])
         
         figure(fcolor)
         subplot(length(labels),1,dip)
-        image(channel)
-        
+        imagesc(channel)
+        title('All trials of ' + string(label) + ' sorted by ' + string(name_test))
+        xlim([800 1700])
+        set(gca,'XTick',[800 900 1000 1100 1200 1300 1400 1500 1600 1700] ); %This are going to be the only values affected.
+        set(gca,'XTickLabel',[-.2 -.1 0 .1 .2 .3 .4 .5 .6 .7] ); %This is what it's going to appear in those places
+
         %plot with offset
 %         offset=5
 %         offset_vector = (offset:offset:nb_bins*offset)';
@@ -219,7 +250,7 @@ for tri = 1:3
         %title('All trials of ' + string(label) + ' binned by ' + string(nb_bins) + ' sorted by ' + string(name_test))
         
         
-        save(['data_' patient_number '_' macro_name '_stats' ], 'fdiff', 'fchan', '-append')
+        save(['data_' patient_number '_' macro_name '_stats' ], 'fdiff', 'fchan', 'peaks_amp_lat', '-append')
 
     end
     pause
